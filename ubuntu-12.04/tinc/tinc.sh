7c045c934171dded3ddf7ac28caa458e13b2ea26
Roman Heinrich <roman.heinrich@gmail.com>
2013-11-10 09:15:22 +0100

    package for tinc

--- ubuntu-12.04/tinc/tinc.sh
+++ ubuntu-12.04/tinc/tinc.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 release_version='1.0.23'
-
+destdir=/tmp/install-tinc-$release_version
 apt-get install -y build-essential
 apt-get install -y texinfo
 apt-get install -y zlibc zlib1g zlib1g-dev
@@ -9,12 +9,29 @@ apt-get install -y liblzo2-dev
 apt-get install -y libssl-dev
 
 
-cd /tmp
+cd /var/tmp
 
 git clone https://github.com/gsliepen/tinc.git
 cd tinc
-git co release-$release_version
+git checkout release-$release_version
 autoreconf -fsi
 ./configure --prefix=
 make
-make install
+make install DESTDIR=$destdir
+
+
+apt-get install ruby rubygems -y
+gem1.8 install fpm --no-ri --no-rdoc
+
+mkdir -p /vagrant/pkg
+cd /vagrant/pkg
+
+fpm -s dir -t deb -n tinc -v $release_version -C $destdir \
+  -p tinc-VERSION_ARCH.deb \
+  -d "zlibc (>= 0.9)" \
+  -d "zlib1g (>= 1:1.2.3.4)" \
+  -d "libssl-dev (>= 1.0.1)" \
+  -d "liblzo2-dev (>= 2.06-1)" \
+  sbin share
+
+rm -rf $destdir
